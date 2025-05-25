782c4432320de754e7e3c3b82e947b85d4e8d8a6
Fragalis <zephyrgeometrydash@gmail.com>
2025-05-25 13:10:56 +0700

    change control_task logic to be event-driven

--- src/tasks/control_task.cpp
+++ src/tasks/control_task.cpp
@@ -2,15 +2,17 @@
 
 static const char *TAG = "CONTROL";
 
-void control_stepper_motor(int state) {
-    gpio_set_level(STEPPER_DIRECTION_PIN, state);
-    int step_count = 0;
-    while (step_count < MAX_STEPS) {
-        step_count++;
-        gpio_set_level(STEPPER_STEP_PIN, 1);
-        vTaskDelay(pdMS_TO_TICKS(STEPPER_DELAY_TIMER));
-        gpio_set_level(STEPPER_STEP_PIN, 0);
-        vTaskDelay(pdMS_TO_TICKS(STEPPER_DELAY_TIMER));
+volatile bool light_state = false;
+
+bool is_dark() {
+    if (data_storage.get_ldr_data().luminosity < LIGHT_THRESHOLD) return 1;
+    return 0;
+}
+
+void check_state_task(void *args) {
+    while (1) {
+        light_state = is_dark();
+        vTaskDelay(pdMS_TO_TICKS(CHECK_STATE_TIMER));
     }
 }
 
@@ -18,54 +20,14 @@ void control_light(int state) {
     gpio_set_level(LIGHT_PIN, state); // Set lamp state
 }
 
-enum CURTAIN_STATE {
+enum DOOR_STATE {
     OPEN, OPENING,
     CLOSE, CLOSING
 };
 
 void control_task(void *arg) {
-    CURTAIN_STATE curtain_state = OPEN; // Initial state
-    uint32_t luminosity = 1;
-    uint8_t hour = 0;
     while (1) {
-        // Get data
-        luminosity = data_storage.get_ldr_data().luminosity;
-        hour = data_storage.get_ntp_data().hour;
-        switch (curtain_state) {
-            case OPEN:
-                // Close the curtain if it's dark or after 5 PM
-                if (luminosity <= LIGHT_THRESHOLD || hour >= 17) {
-                    ESP_LOGI(TAG, "Curtain is closing");
-                    curtain_state = CLOSING;
-                }
-                break;
-            case CLOSING:
-                // Close the curtain
-                control_stepper_motor(1);
-                // Turn on lamp
-                control_light(1);
-                ESP_LOGI(TAG, "Curtain is closed");
-                curtain_state = CLOSE;
-                break;
-            case CLOSE:
-                // Open the curtain if it's light or between 7 AM and 12 PM
-                if (luminosity > LIGHT_THRESHOLD || (hour >= 7 && hour <= 12)) {
-                    ESP_LOGI(TAG, "Curtain is opening");
-                    curtain_state = OPENING;
-                }
-                break;
-            case OPENING:
-                // Open the curtain
-                control_stepper_motor(0);
-                // Turn off lamp
-                control_light(0);
-                ESP_LOGI(TAG, "Curtain is opened");
-                curtain_state = OPEN;
-                break;
-            default:
-                break;
-        }
-        // ESP_LOGI(TAG, "Current curtain state: %d", curtain_state);
+        control_light(light_state);
         vTaskDelay(pdMS_TO_TICKS(CONTROL_TIMER)); // Adjust delay as needed
     }
 }
@@ -73,8 +35,16 @@ void control_task(void *arg) {
 void control_task_init(void) 
 {
     // Create the control task
-    BaseType_t task_created = xTaskCreate(control_task, "control_task", 4096, NULL, 5, NULL);
-    if (task_created != pdPASS) {
+    BaseType_t check_state_task_created = xTaskCreate(check_state_task, "check_state_task", 4096, NULL, 3, NULL);
+    if (check_state_task_created != pdPASS) {
+        ESP_LOGE(TAG, "Failed to create check_state_task");
+        return;
+    }
+    ESP_LOGI(TAG, "check_state_task initialized");
+
+    // Create the control task
+    BaseType_t control_task_created = xTaskCreate(control_task, "control_task", 2048, NULL, 4, NULL);
+    if (control_task_created != pdPASS) {
         ESP_LOGE(TAG, "Failed to create control_task");
         return;
     }
